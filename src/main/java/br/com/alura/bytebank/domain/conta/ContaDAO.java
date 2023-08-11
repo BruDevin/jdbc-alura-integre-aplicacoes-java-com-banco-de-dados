fbfee1566dc47ad054fdcc18a389ca87e982835d
BruDevin <bruno.ads.2022@gmail.com>
2023-08-11 18:26:44 -0300

    JAVA. JDBC. Alura. Formação Integre aplicações Java com Banco de dados. Curso Java e JDBC: trabalhando com um banco de dados. Fim da segunda aula.

---
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
@@ -0,0 +1,41 @@
+package br.com.alura.bytebank.domain.conta;
+
+import br.com.alura.bytebank.domain.cliente.Cliente;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ContaDAO {
+
+    private Connection conn;
+
+    ContaDAO (Connection connection) {
+        this.conn = connection;
+    }
+
+    public void salvar(DadosAberturaConta dadosDaConta) {
+        var cliente = new Cliente(dadosDaConta.dadosCliente());
+        var conta = new Conta(dadosDaConta.numero(), cliente);
+
+        String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email)" +
+                "VALUES (?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, conta.getNumero());
+            ps.setBigDecimal(2, BigDecimal.ZERO);
+            ps.setString(3, dadosDaConta.dadosCliente().nome());
+            ps.setString(4, dadosDaConta.dadosCliente().cpf());
+            ps.setString(5, dadosDaConta.dadosCliente().email());
+
+            System.out.println(ps.execute());
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
