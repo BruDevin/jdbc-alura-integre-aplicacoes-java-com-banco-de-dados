d1f0e5b8dbad76f74e464ab98b13d04f2d46cf7f
BruDevin <bruno.ads.2022@gmail.com>
2023-08-11 20:15:16 -0300

    JAVA. JDBC. Alura. Formação Integre aplicações Java com Banco de dados. Curso Java e JDBC: trabalhando com um banco de dados. Fim da segunda aula.

--- src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
@@ -1,11 +1,15 @@
 package br.com.alura.bytebank.domain.conta;
 
 import br.com.alura.bytebank.domain.cliente.Cliente;
+import br.com.alura.bytebank.domain.cliente.DadosCadastroCliente;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContaDAO {
 
@@ -30,12 +34,79 @@ public class ContaDAO {
             ps.setString(4, dadosDaConta.dadosCliente().cpf());
             ps.setString(5, dadosDaConta.dadosCliente().email());
 
-            System.out.println(ps.execute());
+            ps.execute();
             ps.close();
             conn.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Set<Conta> listar() {
+        Set<Conta> contas = new HashSet<>();
+
+        String sql = "SELECT * FROM conta";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+
+            while(rs.next()) {
+                Integer numero = rs.getInt(1);
+                BigDecimal saldo = rs.getBigDecimal(2);
+                String nome = rs.getString(3);
+                String cpf = rs.getString(4);
+                String email = rs.getString(5);
+                DadosCadastroCliente dadosCadastroCliente =
+                        new DadosCadastroCliente(nome, cpf, email);
+                Cliente cliente = new Cliente(dadosCadastroCliente);
+
+                contas.add(new Conta(numero, cliente));
+            }
+
+            rs.close();
+            ps.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return contas;
+    }
+
+    public Conta listarPorNumero (Integer numero) {
+        String sql = "SELECT * FROM conta WHERE numero = ?";
+        Conta conta = null;
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, numero);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Integer numeroRecuperado = rs.getInt(1);
+                BigDecimal saldo = rs.getBigDecimal(2);
+                String nome = rs.getString(3);
+                String cpf = rs.getString(4);
+                String email = rs.getString(5);
+
+                DadosCadastroCliente dadosCadastroCliente =
+                        new DadosCadastroCliente(nome, cpf, email);
+                Cliente cliente = new Cliente(dadosCadastroCliente);
+
+                conta = new Conta(numeroRecuperado, cliente);
+            }
+
+            rs.close();
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return conta;
     }
 
 }
